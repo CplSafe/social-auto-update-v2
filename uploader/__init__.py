e8a73e00ec24eceaf52862b7c7e02cd98ab43afa
CplSafe <1441333484@qq.com>
2026-04-20 01:34:48 +0800

    fix: fallback BASE_DIR when conf module not on sys.path

--- uploader/__init__.py
+++ uploader/__init__.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
-from conf import BASE_DIR
+try:
+    from conf import BASE_DIR
+except ModuleNotFoundError:
+    BASE_DIR = Path(__file__).parent.parent.resolve()
 
 Path(BASE_DIR / "cookies").mkdir(exist_ok=True)
